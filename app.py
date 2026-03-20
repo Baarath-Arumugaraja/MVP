e54b5ea3f91bf1906dc8202662ce688bc6d93e28
Baarath-Arumugaraja <baarath10@gmail.com>
2026-03-20 12:27:34 +0530

    Refactor import statements in app.py
    Consolidate import statements for sys and os

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import sys
-import os
+import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
+os.chdir(os.path.join(os.path.dirname(__file__), 'backend'))
 from app import app
+
